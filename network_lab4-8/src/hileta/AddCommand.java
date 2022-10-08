a8949670f2f18f9e31aee204fea4c3561c2b1e29
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2022-10-08 21:41:46 +0300

    add new class AddCommand for adding new objects

---
+++ network_lab4-8/src/hileta/AddCommand.java
@@ -0,0 +1,22 @@
+package hileta;
+
+public class AddCommand implements MenuCommand {
+    private String COMMAND_INFO = "add new object";
+    private Network network;
+
+    public AddCommand(Network network) {
+        this.network = network;
+    }
+
+    public void execute() {
+        System.out.println("add");
+        this.network.addAbroad();
+        this.network.addCustomer();
+        this.network.addNumber();
+        (new FromFileCommand(this.network)).execute();
+    }
+
+    public String getCommandInfo() {
+        return this.COMMAND_INFO;
+    }
+}
